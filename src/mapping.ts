cbbc04209dff0885a2700c60705f6834ee7fbe94
jdestephen <jdestephen07@gmail.com>
2019-04-09 12:04:08 -0600

    added Farm entity

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,13 +1,14 @@
 import { LogAddActor, LogUpdateActor } from './types/ActorFactory/ActorFactory'
-import { Actor } from './types/schema'
+import { LogAddFarm, LogUpdateFarm } from './types/FarmFactory/FarmFactory'
+import { Actor, Farm } from './types/schema'
 
 export function handleNewActor(event: LogAddActor): void {
   let actor = new Actor(event.params._id.toHex())
-  actor.name = event.params._name
-  actor.typeOfActor = event.params._typeOfActor
-  actor.country = event.params._country
-  actor.region = event.params._region
-  actor.email = event.params._email
+  actor.name = event.params._name.toString()
+  actor.typeOfActor = event.params._typeOfActor.toString()
+  actor.country = event.params._country.toString()
+  actor.region = event.params._region.toString()
+  actor.email = event.params._email.toString()
   actor.imageHash = event.params._imageHash
   actor.bio = event.params._bio
 
@@ -20,13 +21,42 @@ export function handleUpdatedActor(event: LogUpdateActor): void {
   if (actor == null) {
     actor = new Actor(id)
   }
-  actor.name = event.params._name
-  actor.typeOfActor = event.params._typeOfActor
-  actor.country = event.params._country
-  actor.region = event.params._region
-  actor.email = event.params._email
+  actor.name = event.params._name.toString()
+  actor.typeOfActor = event.params._typeOfActor.toString()
+  actor.country = event.params._country.toString()
+  actor.region = event.params._region.toString()
+  actor.email = event.params._email.toString()
   actor.imageHash = event.params._imageHash
   actor.bio = event.params._bio
 
   actor.save()
+}
+
+
+export function handleNewFarm(event: LogAddFarm): void {
+  let farm = new Farm(event.params._id.toHex())
+  farm.owner = event.params._ownerAddress.toString()
+  farm.name = event.params._name.toString()
+  farm.country = event.params._country.toString()
+  farm.region = event.params._region.toString()
+  farm.village = event.params._village.toString()
+  farm.story = event.params._story
+
+  farm.save()
+}
+
+export function handleUpdatedFarm(event: LogUpdateFarm): void {
+  let id = event.params._id.toHex()
+  let farm = Farm.load(id)
+  if (farm == null) {
+    farm = new Farm(id)
+  }
+  farm.owner = event.params._ownerAddress.toString()
+  farm.name = event.params._name.toString()
+  farm.country = event.params._country.toString()
+  farm.region = event.params._region.toString()
+  farm.village = event.params._village.toString()
+  farm.story = event.params._story
+
+  farm.save()
 }
